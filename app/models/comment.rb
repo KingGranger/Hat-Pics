7c783890181570b16e0fbce31103dc7ea6139ea3
Brandon Harris <cheeridon@gmail.com>
2017-11-13 15:31:01 -0500

    validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :hat_pic
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, :length {maximum: 140}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   has_many :hats
   has_many :hat_pics, through: :hats
 
-  has_secure_password
-
-  #validates_confirmation_of :password
+  validates_confirmation_of :password
+  validates :username, presence: true, uniqueness: true, :length {in: 4..16}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates :password, presence:true, length: {minimum:5}
 end
