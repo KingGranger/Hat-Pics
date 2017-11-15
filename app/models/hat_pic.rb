3f635fd13810eb4221f456359eb3759902f321e9
github username <github email address>
2017-11-15 15:57:26 -0500

    added more styling

--- app/models/hat_pic.rb
+++ app/models/hat_pic.rb
@@ -4,8 +4,14 @@ class HatPic < ApplicationRecord
   # has_many :raters, through: :ratings, class_name: 'User' #paperclip gem
   has_many :comments
   # has_many :commenters, through: :comments, class_name: 'User'
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", large: "500x500>"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  def scores
+    self.ratings.map {|rating| rating.score}
+  end
 
+ def average_rating
+    scores.reduce(:+) / scores.size
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :hats
   has_many :hat_pics, through: :hats
-
+  has_secure_password
   validates_confirmation_of :password
   validates :username, presence: true, uniqueness: true, length: {in: 4..16}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates :password, presence:true, length: {minimum:5}
-  attr_accessor :password, :password_confirmation
+
 
 end
