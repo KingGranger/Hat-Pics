1f7eafe42be5789a4fbf0abbc60f6e28908f21dc
github username <github email address>
2017-11-15 09:35:55 -0500

    got picture working

--- app/models/hat_pic.rb
+++ app/models/hat_pic.rb
@@ -1,11 +1,9 @@
 class HatPic < ApplicationRecord
   belongs_to :hat
   has_many :ratings
-  # has_many :raters, through: :ratings, class_name: 'User' #paperclip gem
   has_many :comments
-  # has_many :commenters, through: :comments, class_name: 'User'
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
-
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   def scores
     self.ratings.collect {|rating| rating.score}
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
 lina = User.find_or_create_by(username: 'lina', email: "hello", password_digest: "123")
 james = User.find_or_create_by(username: 'james')
 brandon = User.find_or_create_by(username: 'brandon')
 hat1 = Hat.find_or_create_by(user: lina, color: 'red')
 hat2 = Hat.find_or_create_by(user: james, color: 'purple')
-hatpic1 = HatPic.find_or_create_by(hat: hat1)
-hatpic2 = HatPic.find_or_create_by(hat: hat2)
+
+file1 = File.new(File.join(Rails.root, 'app', 'assets', 'images', 'seed', 'hat.jpg'))
+
+
+
+hatpic2 = HatPic.create(hat: hat2)
+
+hatpic1 = HatPic.create(hat: hat1, avatar: file1)
+
+
+
+
 Rating.find_or_create_by(user: lina, hat_pic: hatpic1, score: 5)
 Rating.find_or_create_by(user: james, hat_pic: hatpic2, score: 3)
 
