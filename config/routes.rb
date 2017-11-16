22d33fd6ac96dd6042f3faeb4b98b8970ce50190
github username <github email address>
2017-11-16 10:04:53 -0500

    committing to pull

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  get "/", to: "hat_pics#index", as: "root"
 
   resources :hats
   resources :comments, only: [:create, :destroy]
